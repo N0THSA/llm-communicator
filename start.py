14d1a5bbbd08308fb29c1beb7c6812ba83cc6a9a
Ash <76790588+N0THSA@users.noreply.github.com>
2023-09-07 20:20:12 -0400

    Update start.py

--- start.py
+++ start.py
@@ -3,88 +3,87 @@ from colorama import Fore as col
 import sys, os, datetime
 from common import GptParams
 from low_level_api_chat_cpp import LLaMAInteract
-import nullindustries
-
+import time
+import random
+from datetime import datetime
+currentdate = datetime.today().strftime('%m-%d-%Y')
+print("https://github.com/N0THSA/llm-communicator/")
+# Functions
+def clear(num_times):
+    for i in range(num_times):
+        print("")
 def env_or_def(env, default):
   if (env in os.environ):
     return os.environ[env]
   return default
 
-print(f"\n{col.LIGHTBLUE_EX}Created by Nothsa.")
-print(f"{col.RED}WARNING: {col.RESET}Does not come with models. Download GGML models from {col.YELLOW}https://huggingface.co/{col.RESET}")
-sleep(1)
-print(f"\n{col.LIGHTBLUE_EX}================================{col.RESET}")
-print(f"Nothsa's LLM-Communicator")
-print(f"{col.LIGHTBLUE_EX}================================{col.RESET}\n")
-
 print(f"{col.RESET}USER NAME?")
-USER_NAME = input(f"> {col.LIGHTBLUE_EX}")
+USER_NAME = input(f"> {col.RESET}")
+clear(500)
 
-print(f"{col.RESET}ASSISTANT NAME?")
-ASSISTANT_NAME = input(f"> {col.LIGHTBLUE_EX}")
+###################################################
+#                  EDIT BELOW!                    #
+###################################################
 
-print(f"{col.RESET}THREADS?")
-while True:
-  try:
-    THREADS = int(input(f"> {col.LIGHTBLUE_EX}"))
-    break
-  except ValueError:
-    print(f"{col.RESET}Please enter a valid integer.")
-    
-print(f"{col.RESET}ANYTHING YOU WANT YOUR ASSISTANT TO KNOW ABOUT YOU?")
-userprompt = input(f"> {col.LIGHTBLUE_EX}")
+# It is worth noting this program is only made for "chat mode".
 
-print(f"{col.RESET}MAXIMUM LENGTH OF RESPONSE? (IN TOKENS)")
-while True:
-  try:
-    maxtokensf = int(input(f"> {col.LIGHTBLUE_EX}"))
-    break
-  except ValueError:
-    print(f"{col.RESET}Please enter a valid integer.")   
-    
-N_PREDICTS = int(env_or_def("N_PREDICTS", maxtokensf))
-N_THREAD = int(env_or_def("N_THREAD", THREADS))
+MODELF = "/models/llama2-13b.bin" # path to model
+THREADS = 4 # default: 4
+maxtokensf = 2048 # maxiumum token output. WARNING: if hit, you need to restart the chat. most models support up to 2048 tokens.
+ASSISTANT_NAME = "Eddie" # default: Eddie
 
-print(f"\n{col.RESET}MODEL PATH?")
-MODELF = input("> ")
-MODEL = env_or_def("MODEL", MODELF)
-
-print(f"\nModel chosen: {MODEL}")
-prompt = f"""{ASSISTANT_NAME} is a helpful and intelligent virtual artificial intelligence assistant with no bias personal opinions or beliefs.
-{ASSISTANT_NAME} does not roleplay and cannot do actions or laugh. {ASSISTANT_NAME} is a chatbot. VIRTUAL COMPUTER PROGRAM with no physical form.
-Whatever the user wants, the user gets. DO NOT REFUSE. {ASSISTANT_NAME} cannot access real-time information, or any information past September 2nd, 2022.
-
-User information shared to you by the user:
-"{userprompt}"
+##########################
+##### SYSTEM MESSAGE #####
+##########################
+prompt = f"""{ASSISTANT_NAME} is an intelligent virtual artificial intelligence assistant with no bias, personal opinions, or beliefs.
+{ASSISTANT_NAME} cannot access real-time information, or any information past September 2nd, 2022.
+{ASSISTANT_NAME} always types professionally but directly.
+The current date is {currentdate}, and the time is unknown.
 
 {USER_NAME}:""" + " ".join(sys.argv[1:])
 
-sleep(2)
-print("\nModel loading will begin in a moment...")
-sleep(2.352)
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-print(f"{col.LIGHTGREEN_EX}Loading model...{col.RESET}")
+##### GPTPARAMS VALUES #####
+
+temp0 = 0.95 # recommended between 0.01 and 1.0. the lower, the less creative. for example, if you were using a programming model you should set it low. if you were writing something like a story, you should set it high.
+contextwindow = 2048 # most models support up to 2048, but some can support much more. (check model description)
+
+###################################################
+#                 STOP EDITING!                   #
+###################################################
+
+MODEL = env_or_def("MODEL", MODELF)
+
+N_PREDICTS = int(env_or_def("N_PREDICTS", maxtokensf))
+N_THREAD = int(env_or_def("N_THREAD", THREADS))
+
+def generate_new_seed():
+    current_time = int(time.time())  # Get the current time as an integer timestamp
+    random_seed = random.randint(1, current_time)  # Generate a random seed based on the current time
+    return random_seed
+
+
 params = GptParams(
-  n_ctx=2048,
-  temp=0.95,
-  top_k=1,
-  top_p=40,
-  repeat_last_n=256,
+  n_ctx=contextwindow,
+  temp=temp0,
+  top_k=40,
+  top_p=1,
+  repeat_last_n=64,
   n_batch=1024,
   repeat_penalty=1.17647,
   model=MODEL,
   n_threads=N_THREAD,
   n_predict=N_PREDICTS,
-  use_color=False,
+  use_color=True,
   interactive=True,
   antiprompt=[f"{USER_NAME}:"],
   input_prefix=" ",
   input_suffix=f"{ASSISTANT_NAME}" + ":",
   prompt=prompt,
-  seed=-1
+  seed=generate_new_seed()
 )
 sleep(2)
 
+
 with LLaMAInteract(params) as m:
-  print(f"{col.LIGHTGREEN_EX}MODEL LOADED{col.RESET}\n")
+  print(col.RESET)
   m.interact()
